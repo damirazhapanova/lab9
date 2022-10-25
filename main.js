58219f78205449ef144c8947429b0a6d071cd0e6
damirazhapanova <damira.zhapanova@nu.edu.kz>
2022-10-25 23:03:15 +0600

    Commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// 13.  Write a function that returns the greatest among the given two numbers.
+let x = 7
+let y = 9
+function greatest(x, y){
+  if(x > y){
+    console.log("x is the greatest among the given two numbers")
+  } else {
+    console.log("y is the greatest among the given two numbers")
+  }
+}
+
+
+// 11.  Write a function to check whether a given number is even or odd.
+let x = 5
+if(x & 2 == 0){
+  console.log("x is even")
+} else {
+  console.log("x is odd")
+}
+
+
+//4.  Write a function that greets a person by his/her name (e.g. “Hi, John!”).
+public static void main(String[] args) {
+
+String myGreeting = greet("Hi, John!");
+}
+
+public static String greet(String name, String greeting) {
+return greeting + ", " + name + "!";
+}
